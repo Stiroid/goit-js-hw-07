dd4c35edf4108723dc7194d5ea6770c52906a869
Stiroid <stakhovsky89@gmail.com>
2023-02-26 13:38:37 +0200

    complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const phContainer = document.querySelector('.gallery');
+const phCards = createPhCards(galleryItems);
+
+
+
+phContainer.addEventListener('click', onClick);
+function onClick(evt) {
+    evt.preventDefault();
+    const instance = basicLightbox.create(`
+   
+    <img src="${evt.target.dataset.source}">
+    
+`)
+instance.show()
+    console.log(evt);
+}
+
+phContainer.insertAdjacentHTML('beforeend', phCards);
+console.log(phContainer);
+
+function createPhCards (photos) {
+return photos.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+        `;
+    }).join('');
+
+} 
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+
+
+const phContainer = document.querySelector('.gallery');
+const phCards = createPhCards(galleryItems);
+phContainer.insertAdjacentHTML('beforeend', phCards);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionSelector : 'img',
+     captionType : 'attr', 
+     captionData : 'alt', 
+     captionPosition : 'bottom', 
+     captionDelay: '250'
+});
+
+function createPhCards (photos) {
+return photos.map(({preview, original, description}) => {
+        
+        return `
+        <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+        `;
+    }).join('');
+
+} 
